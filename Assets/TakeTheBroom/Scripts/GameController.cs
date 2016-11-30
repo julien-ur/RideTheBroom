d668f6aeb24c6cea654786b3ed076a2ee3f4412e
julien-ur <julien.wachter@stud.uni-regensburg.de>
2016-11-30 15:12:40 +0100

    Added: basic score system

---
+++ Assets/TakeTheBroom/Scripts/GameController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject route;
+    public Transform[] ringList;
+    public MagicalRing nextRing;
+    public Text pointDisplay;
+    public int rings;
+    public int ringCount = 0;
+
+    void Start () {
+        ringList = route.GetComponentsInChildren<Transform>();
+        rings = ringList.Length-1;
+        nextRing = ringList[ringCount+1].GetComponent<MagicalRing>();
+        pointDisplay.text = ringCount + "/" + rings;
+    }
+
+    void Update ()
+    {
+        if(nextRing.activated)
+        {
+            next();
+        }
+    }
+	
+	public void next ()
+    {
+        ringCount++;
+
+        if (ringCount >= rings)
+        {
+            pointDisplay.text = "I'm done..";
+        }
+        else
+        {
+            nextRing = ringList[ringCount+1].GetComponent<MagicalRing>();
+            pointDisplay.text = ringCount + "/" + rings;
+        }
+    }
+}
---
+++ Assets/TakeTheBroom/Scripts/MagicalRing.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MagicalRing : MonoBehaviour {
+
+    public bool activated = false;
+
+    void OnTriggerStay(Collider other)
+    {
+        activated = true;
+    }
+}

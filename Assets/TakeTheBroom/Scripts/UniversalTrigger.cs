57f620337b0be1b8201fc5814e3b10ebcd2bbcd4
julien-ur <julien.wachter@stud.uni-regensburg.de>
2016-12-19 17:20:03 +0100

    Added: #1 falling boulder mechanics in testworld

---
+++ Assets/TakeTheBroom/Scripts/UniversalTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UniversalTrigger : MonoBehaviour {
+
+    public GameObject boulders;
+    private Rigidbody[] rbList;
+
+    void Start () {
+        rbList = boulders.GetComponentsInChildren<Rigidbody>();
+        foreach (Rigidbody rb in rbList)
+        {
+            rb.isKinematic = true;
+        }
+    }
+
+    void OnTriggerEnter () {
+        foreach (Rigidbody rb in rbList)
+        {
+            rb.isKinematic = false;
+        }
+    }
+}

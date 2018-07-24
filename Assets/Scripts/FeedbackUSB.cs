c722798171a29dce0107597d4ce861a05d446b8f
Julien Wachter <julien.wachter@stud.uni-regensburg.de>
2018-07-24 14:41:45 +0200

    added USB feedback to Unity

---
+++ Assets/Scripts/FeedbackUSB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeedbackUSB : MonoBehaviour {
+
+    public const int SMELL_LEMON_VAL = 3;
+    public const int SMELL_WOODY_VAL = 1;
+    public const int SMELL_BERRY_VAL = 4;
+
+    public const string WIND_TAG = "w";
+    public const string HEAT_TAG = "h";
+    public const string SMELL_TAG = "s";
+    public const string VIBRATION_TAG = "v";
+
+    public void UpdateFeedback(string rawData, Action callback)
+    {
+
+    }
+}
